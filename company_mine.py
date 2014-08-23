109737d1869a571f19905df9bca19ea107e80482
cinquemb <cinque_mcfarlane-blake@brown.edu>
2014-08-23 19:44:51 -0400

    adding companies to top line item for each county

--- company_mine.py
+++ company_mine.py
@@ -30,17 +30,27 @@ cache_item_dict = dict()
 size = 0
 query_size = 0
 data_size = 0
+company_tot = 0
+company_count = 0
 for node in raw_data_json_['2006-2014-foia-gov-surplus-mil']:
 	for key, value in node.iteritems():
 		county_keys = node[key]['counties_meta_data'].keys()
 		for county in county_keys:
 			query = node[key]['counties_meta_data'][county]['item']
-			if query not in cache_item_dict:
+
+			if node[key]['counties_meta_data'][county]['item_manufacturer'] != 'N/A':
+				company_count += 1
+				company_tot += 1
+			else:
+				company_tot += 1
+
+				
+			if query not in cache_item_dict and node[key]['counties_meta_data'][county]['item_manufacturer'] == 'N/A':
 				stripped_query = re.sub('-', '', query).replace(' ', '')
 
 				url = 'http://www.justnsnparts.com/nsnpart/%s.aspx' % (stripped_query)
 				url2 = 'http://www.govliquidation.com/nsn_info/%s/%s.html' % (stripped_query[:5], stripped_query)
-				url3 = 'http://www.parttarget.com/formprocessor.aspx?formid=top_search_form&m=875&r=aHR0cDovL3d3dy5wYXJ0dGFyZ2V0LmNvbS9pbmRleC5hc3B4&userguid=BDEF4FE9-D508-4554-9D92-1170070BE59A&pagename=&searchtype=0&callid=a26d3a06-a428-43f8-8285-688f072bf444&asyncsearchurl=advancedsearchasync.aspx&searchtext=%s&originalsearchtext=&searchOption=sku' % (stripped_query)
+				url3 = 'http://buyaircraftparts.com/nsn/%s' % (query)
 
 				r = requests.get(url)
 				
@@ -68,31 +78,39 @@ for node in raw_data_json_['2006-2014-foia-gov-surplus-mil']:
 					try:
 						company = soup2.find_all('a', href=re.compile("JavaScript: runSearch\('company','"))[0].string
 						print 'Hit 2! ', stripped_query, 'Length of string: ', len(stripped_query)
+
 						company_info = 'N/A'
 						data_size += 1
 					except IndexError, e:
-						
-						r3 = requests.get(url3)
-						soup3 = BeautifulSoup(r3.text)	
 
+						r3 = requests.get(url3)
+						soup3 = BeautifulSoup(r3.text)
 						try:
-							company = soup3.find_all('table', class_="table-catalog table-part table-index-5 table-supplier")[0].find_all('td', class_="pos_3")[0].find('a').string
-							print 'Hit 3! ', stripped_query, 'Length of string: ', len(stripped_query), 'company ', company
+							node_ = soup3.find_all('a', href=re.compile("/manufacturers/[0-9]+"))[0].string
+							node_url = 'http://buyaircraftparts.com/manufacturers/%s' % (node_)
+
+							#print node_url
+							#sys.exit()
+							
+							r3_n = requests.get(node_url)
+							split_string = 'Cage Code %s | Buy Aircraft Parts' % (node_)
+							company = BeautifulSoup(r3_n.text).title.string.split(split_string)[0]
+							print company
+							print 'Hit 3! ', stripped_query, 'Length of string: ', len(stripped_query)
+
 							company_info = 'N/A'
 							data_size += 1
 						except IndexError, e:
-							if '403 - Forbidden: Access is denied.' in 	r3.text:
-								print 'cookies are bad'	
-							print 'error', r3.status_code
 							print 'Miss! ', stripped_query, 'Length of string: ', len(stripped_query)
+							#print "{%s:''}" % (stripped_query)
 							company = 'N/A'
 							company_info = 'N/A'
-							#sys.exit()					
+			
 
 				size += 1
 
 				cache_item_dict[query] = [company,company_info]
-			else:
+			elif node[key]['counties_meta_data'][county]['item_manufacturer'] == 'N/A':
 				#print 'cache used'
 				query_size += 1
 				size += 1
@@ -103,12 +121,21 @@ for node in raw_data_json_['2006-2014-foia-gov-surplus-mil']:
 			
 			#print company
 			#print company_url
-			node[key]['counties_meta_data'][county]['item_manufacturer'] = company
-			node[key]['counties_meta_data'][county]['item_manufacturer_meta_data'] = company_info
+			if node[key]['counties_meta_data'][county]['item_manufacturer'] == 'N/A':
+				node[key]['counties_meta_data'][county]['item_manufacturer'] = company
+			elif node[key]['counties_meta_data'][county]['item_manufacturer'][-2:] == '- ':
+				node[key]['counties_meta_data'][county]['item_manufacturer'] = node[key]['counties_meta_data'][county]['item_manufacturer'][:-2]
+			
 
-data = json.dumps(raw_data_json_)
+			
+			if node[key]['counties_meta_data'][county]['item_manufacturer_meta_data'] == 'N/A' and company_info:
+				node[key]['counties_meta_data'][county]['item_manufacturer_meta_data'] = company_info
+			
+
+#data = json.dumps(raw_data_json_)
 f = open('2006-2014-foia-gov-surplus-mil.json', 'w+')
 f.write(data)
 f.close()
 print 'Cache Used %s percent of time' % (round_format(query_size, size))
 print 'Companies Found %s percent of time' % (round_format(data_size, size))
+print 'Companies Found %s percent of Total Items' % (round_format(company_count, company_tot))
